88d2b6ad15e08fddb2f9c9fddbd1407ec26f08c1
Victoria <torichamberlain1@gmail.com>
2020-05-27 21:27:21 -0700

    'commit'

--- productsApp/views.py
+++ productsApp/views.py
@@ -8,10 +8,6 @@ def showProduct(request, productId):
     # include context dictionary to pass in a product object
     return HttpResponse('product detail') # render product detail page
 
-def showShoppingCart(request, userId):
-    # include context dictionary to pass the shopping cart object linked to the user
-    return HttpResponse('shopping cart') # render shopping cart page
-
 def showPayment(request, userId):
     return HttpResponse('payment page') # render payment page with all of the items from shopping cart
 
@@ -19,12 +15,45 @@ def processPayment(request, userId):
     # code to process payment
     return HttpResponse('process payment') # process the payment and redirect to the receipt page
 
-def showReceipt(request, orderId):
-    # include context dictionary to pass in the order object
-    return HttpResponse('receipt page') # render receipt page with the order information
-
-def showWishList(request, userId):
+## Wish List
+def wishList(request, userID):
     # include context dictionary to pass the wish list object linked to the user
-    return HttpResponse('wish list page') # render wish list page with all the products and qty
+    return HttpResponse("wish_list.html")
 
+def addWishItem(request, WishList_id):
+    WishList.objects.get(id=wishList_id).products.add(Product.objects.get(id=request.POST['product_id']))
 
+    return redirect('/wishlist')
+
+def deleteWishItem(request, product_id):
+    to_delete=wishList.objects.get(id=product_id)
+    to_delete.delete()
+    return redirect('/wishlist')
+
+def addWishtoCart(request, ShoppingCart_id):
+    ShoppingCart.objects.get(id=ShoppingCart_id).products.add(Product.objects.get(id=request.POST['product_id']))
+
+    return redirect('/wishlist')
+    ##this cannot be right...
+
+##Shopping Cart
+def shoppingCart(request, user_id):
+    # include context dictionary to pass the shopping cart object linked to the user
+    return HttpResponse("shopping_cart.html")
+
+def deleteCartItem(request, product_id):
+    to_delete=ShoppingCart.objects.get(id=product_id)
+    to_delete.delete()
+    return redirect('/shoppingcart')
+
+def addToCart(request, ShoppingCart_id):
+     ShoppingCart.objects.get(id=ShoppingCart_id).products.add(Product.objects.get(id=request.POST['product_id']))
+
+    return redirect('/shoppingcart')
+
+##Receipt
+def receipt(request, order_id):
+    context={
+        "purchased_items": Order.objects.get(id=order_id)
+    }
+    return HttpResponse('receipt.html')
