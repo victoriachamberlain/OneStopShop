b9036293aeffdf33a6607257a45407adcc70ab12
donaldleblanc9150 <donald.leblanc@gmail.com>
2020-05-28 17:58:25 -0700

    admin panel fixed

---
+++ Desktop/Dojo_projects/OneStopShop/productsApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('products/show/<int:productId>', views.showProduct),
+    path('user/<int:userId>/shoppingCart', views.showShoppingCart),
+    path('user/<int:userId>/payment', views.showPayment),
+    path('user/<int:userId>/payment/process', views.processPayment),
+    path('<int:orderId>/receipt', views.showReceipt),
+    path('user/<int:userId>/wishList', views.showWishList),
+]
